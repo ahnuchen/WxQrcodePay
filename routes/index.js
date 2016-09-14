25b0ea5d3745703cc387bb23ca66b2141972ab16
jakeyChen1994 <1292572728@qq.com>
2016-09-14 13:56:09 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '自动售货机' });
+  res.render('index', { title: '微信支付' });
 });
 
 module.exports = router;
